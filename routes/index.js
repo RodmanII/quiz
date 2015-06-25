4cafb7bd75cd1e420e48ba8a3a0ec427f7db63da
Rodrigo Grijalva <airodrigolopez@hotmail.com>
2015-06-25 15:57:30 -0600

    Definido load en quiz_controller, correcion

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.param('quizId', quizController.load);
+
 router.get('/author', function(req, res, next){
   res.render('author');
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
